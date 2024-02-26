e8d6aba5c1cd9cf312728ce96e96f2c3f74da58c
Dragon_Fly <143097451+gurpreetsinghwazir@users.noreply.github.com>
2024-02-26 17:13:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+a href="#">Popular Bootcamps</a>		
+		<a href="#">Bootcamp Developers</a>
+		<a href="#">Bootcamp Financing</a>
+		<a href="#">Bootcamp Grads</a>
+		<a href="#"> Alumni</a>
