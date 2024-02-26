fed94d85acbb9a4d0ff850fa0b0c3b793dd48870
Dragon_Fly <143097451+gurpreetsinghwazir@users.noreply.github.com>
2024-02-26 17:13:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,3 +6,6 @@ a href="#">Popular Bootcamps</a>
 		<a href="#">Bootcamp Financing</a>
 		<a href="#">Bootcamp Grads</a>
 		<a href="#"> Alumni</a>
+
+
+// https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_dropdown_navbar_click
