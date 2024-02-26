928341e836c3bbf809332e137f6e01466b676fd8
Dragon_Fly <143097451+gurpreetsinghwazir@users.noreply.github.com>
2024-02-26 22:16:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
-a href="#">Popular Bootcamps</a>		
-		<a href="#">Bootcamp Developers</a>
-		<a href="#">Bootcamp Financing</a>
-		<a href="#">Bootcamp Grads</a>
-		<a href="#"> Alumni</a>
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+  var myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
+    }
+  }
+}
+
 
 
 // https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_dropdown_navbar_click
+
+
+
